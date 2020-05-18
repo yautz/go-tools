2efc1bf11bb3ba6915f14a9826e9d9a2b7c10b4e
YauTz <s11037037yau@gmail.com>
2020-05-18 17:43:37 +0800

    Add ConsulKV func

---
+++ config/consul/remoteKV.go
@@ -0,0 +1,52 @@
+package consul
+
+import (
+	"bytes"
+
+	"github.com/hashicorp/consul/api"
+	"github.com/spf13/viper"
+)
+
+// ConsulKV -  拆出consul Key/Value
+func ConsulKV(host, key string, ftype string) (*viper.Viper, error) {
+	v := viper.New()
+
+	c := &api.Config{
+		Address: host,
+		Scheme:  "http",
+	}
+
+	// connect consul server
+	client, err := api.NewClient(c)
+
+	if err != nil {
+		panic("connect" + host + "failed")
+		return nil, err
+	}
+
+	// Get a handle to the KV API
+	kv := client.KV()
+
+	pair, _, err := kv.Get(key, nil)
+
+	if err != nil {
+		panic("KV Data Not Found")
+		return nil, err
+	}
+
+	// viper type set
+	switch ftype {
+	case "yaml":
+		v.SetConfigType("yaml")
+	case "json":
+		v.SetConfigType("json")
+	}
+
+	err = v.ReadConfig(bytes.NewReader(pair.Value))
+
+	if err != nil {
+		panic("read consul config failed with key :" + key)
+		return nil, err
+	}
+	return v, nil
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/yautz/go-tools
+
+go 1.14
+
+require (
+	github.com/spf13/viper v1.7.0
+	github.com/hashicorp/consul/api v1.1.0
+)
