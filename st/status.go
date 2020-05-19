34d6aa981f8ee64a01b2b13fd5a64a9bc574e1b0
YauTz <s11037037yau@gmail.com>
2020-05-19 20:11:45 +0800

    st end

--- st/status.go
+++ st/status.go
@@ -1,22 +1,54 @@
 package st
 
 import (
+	"fmt"
+
 	"google.golang.org/grpc/codes"
+	gs "google.golang.org/grpc/status"
 )
 
-func NewError(code int32, msg string, gc codes.Code) *err {
-	if gc == codes.OK {
-		return nil
-	}
+func NewError(code int32, msg string, gc codes.Code) error {
 	x := &body{
 		Code: code,
 		Msg:  msg,
 	}
 
-	err := &err{
-		gst: gc,
+	str, _ := json.Marshal(x)
+	s := &err{
+		gst: gs.New(gc, string(str)),
 		st:  x,
 	}
 
+	err := s.Err()
+
 	return err
+}
+
+// ConvertError -
+func ConvertError(errs error) Errors {
+	// success
+	if errs == nil {
+		return &err{
+			gst: gs.New(OK, "success"),
+			st:  &body{Code: 0, Msg: "success"},
+		}
+	}
+
+	gt, ok := gs.FromError(errs)
+
+	temp := &err{
+		gst: gt,
+	}
+
+	if ok {
+		// 如果不能解析則給他錯誤代碼
+		if err := temp.parseMsg(); err != nil {
+			temp.st.Code = 99999
+			temp.st.Msg = gt.Message()
+		}
+	} else {
+		fmt.Println("convert error failed with errr : %s", errs.Error())
+	}
+
+	return temp
 }
--- st/status_test.go
+++ st/status_test.go
@@ -4,16 +4,15 @@ import (
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
-	// "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_NewErr(t *testing.T) {
-
 	err := NewError(0, "success", OK)
 	spew.Dump(err)
-	// assert.NoError(t, err)
+	assert.NoError(t, err)
 
 	err = NewError(1000, "NotFound", NotFound)
 	spew.Dump(err)
-	// assert.Error(t, err, "NotFound")
+	assert.Error(t, err, "NotFound")
 }
